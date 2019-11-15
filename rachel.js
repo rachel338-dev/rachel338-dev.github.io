157fa8ccd230b8c0b0fc01d8b5d42f2357dbe3f0
Rachel Owusu-Ansah <rachel.owusuansah@shopify.com>
2019-11-15 13:18:29 -0500

    adding all files

---
+++ rachel.js
@@ -0,0 +1,35 @@
+
+
+function replace() {
+    var pic = document.getElementById("vid1");
+    pic.innerHTML = "<video controls autoplay src='outnumber.mp4'></video>";
+}
+
+
+function mouseItem(){
+    var list = document.getElementById("list");
+    list.innerHTML = "<li>E-Commerce store created on Shopify Platform.</li> <li>For the fashion brand, OUTNUMBER.</li> <li>Opened S/S2019 for Pre-orders.</li> <li>Languages used: HTML, CSS, Javascript & Liquid.</li>";
+}
+function mouseItem1(){
+    var oopsie = document.getElementById("oopsie");
+oopsie.innerHTML = "OOpsie crazy project: This project is a RPG-like object oriented program." ;
+
+
+}
+
+function mouseItem2(img1){
+var ascii = document.getElementById("ascii");
+ascii.innerHTML = "ASCII Art";
+
+
+}
+function mouseItem3(img2){
+var cashmoney = document.getElementById("cashmoney");
+cashmoney.innerHTML = "Cash Money: A program that calculates the minimum number of coins <br>required to give a user change."; 
+
+}
+
+
+
+
+
